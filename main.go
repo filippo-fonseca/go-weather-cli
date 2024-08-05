4ece1677312354ddb8be0246f6c7365d2c72ea4d
Filippo Fonseca <filifonsecacagnazzo@gmail.com>
2024-08-05 17:09:06 -0600

    status check

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
+	res, err := http.Get("http://api.weatherapi.com/v1/forecast.json?key=f339f0fe0fa9486da65230533240508&q=London")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		panic("API request failed")
+	}
 	fmt.Println("ready to go")
 }
